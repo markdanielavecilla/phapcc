58b2fe3724752f8edf9bcfed15cb9875c142ea86
unknown <markdanielavecilla@gmail.com>
2022-11-23 15:54:35 +0800

    removed font awesome stylesheet on each files and put it on navigation file instead

--- admin/includes/navigation.php
+++ admin/includes/navigation.php
@@ -1,5 +1,6 @@
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css" rel="stylesheet">
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-select/1.14.0-beta2/css/bootstrap-select.min.css"/>
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css"/>
 
 <nav class="navbar navbar-dark navbar-expand-lg bg-dark">
     <div class="container">
