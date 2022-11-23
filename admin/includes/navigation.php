51bb5476b3614037b4b3720fa2fd1b15a38a93d0
unknown <markdanielavecilla@gmail.com>
2022-11-23 15:50:29 +0800

    added return link

--- admin/includes/navigation.php
+++ admin/includes/navigation.php
@@ -1,47 +1,25 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css" rel="stylesheet">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-select/1.14.0-beta2/css/bootstrap-select.min.css"/>
-    <title>Document</title>
-</head>
-<body>
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css" rel="stylesheet">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-select/1.14.0-beta2/css/bootstrap-select.min.css"/>
 
+<nav class="navbar navbar-dark navbar-expand-lg bg-dark">
+    <div class="container">
+        <a class="navbar-brand" href="./index.php">
+            Admin Panel
+        </a>
 
-    <nav class="navbar navbar-dark navbar-expand-lg bg-dark">
-        <div class="container">
-            <a class="navbar-brand" href="./index.php">
-                Admin Panel
-            </a>
-
-            <ul class="navbar-nav d-flex">
-                <?php
-                    if(isset($_SESSION['admin_auth'])) :
-                ?>
-                    <li class="nav-item">
-                        <a class="nav-link" aria-current="page" href="./dashboard.php">Home</a>
-                    </li>
-                    <!-- <li class="nav-item">
-                        <a class="nav-link" href="./chapter-list.php">Chapters</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="#">Special Training</a>
-                    </li> -->
-                    <li class="nav-item">
-                        <a class="nav-link" href="./includes/logout.php">Logout</a>
-                    </li>
-                <?php
-                    endif;
-                ?>
-            </ul>
-            
-        </div>
-    </nav>
-
-    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/js/bootstrap.bundle.min.js"></script>
-</body>
-</html>
+        <ul class="navbar-nav d-flex">
+            <?php
+                if(isset($_SESSION['admin_auth'])) :
+            ?>
+                <li class="nav-item">
+                    <a class="nav-link" aria-current="page" href="./dashboard.php">Home</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="./includes/logout.php">Logout</a>
+                </li>
+            <?php
+                endif;
+            ?>
+        </ul>  
+    </div>
+</nav>
--- admin/view-users.php
+++ admin/view-users.php
@@ -17,6 +17,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css" integrity="sha512-xh6O/CkQoPOWDdYTDqeRdPCVd1SpvCA9XXcUnZS2FmJNp1coAFzvtCN9BmamE+4aHK8yyUHUSCcJHgXloTyT2A==" crossorigin="anonymous" referrerpolicy="no-referrer" />
     <title>All users</title>
 </head>
 <body>
@@ -27,7 +28,18 @@
     </section>
     <main>
         <div class="container my-5">
-            <h1>All users</h1>
+            <div class="row">
+                <div class="col-md-12">
+                    <h2>
+                        All users
+                        <span>
+                            <a href="./dashboard.php" class="btn btn-outline-danger float-end">
+                                <i class="fa fa-arrow-left" aria-hidden="true"></i>
+                            </a>
+                        </span>
+                    </h2>
+                </div>
+            </div>
             <hr/>
             <div class="row">
                 <table class="table">
